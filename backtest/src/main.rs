4d7cd355223f312c95ea656a17b6a4a6b1e9aded
Jonty <jonty.morris@yahoo.co.nz>
2020-11-16 01:25:28 +1300

    Updated the output and range

--- backtest/src/main.rs
+++ backtest/src/main.rs
@@ -9,13 +9,9 @@ use stocks_core::util::parse_date;
 fn main() {
     let step_size = 3;
 
-    println!("Loading stocks...");
+    println!("Preparing stocks...");
     let stocks = get_all_stocks("us".into());
-
-    println!("Averaging changes...");
-    let changes = get_all_average_changes(&stocks, step_size);
-
-    println!("Generating bounds...");
+    let changes = get_all_average_changes(&stocks, 7);
     let bounds = get_all_bounds(&changes);
 
     let config = Config {
@@ -23,22 +19,23 @@ fn main() {
         buy_amount: 100.0,
         sell_loss_percent: 5.0,
         step_size: step_size,
-        start_date: parse_date("2018-09-01".to_string()),
-        end_date: parse_date("2020-11-15".to_string()),
+        start_date: parse_date("2015-01-01".to_string()),
+        end_date: parse_date("2019-10-01".to_string()),
     };
 
-    println!("Performing simulation...");
+    println!("Running simulation...");
     let result = perform_simulation(stocks, bounds, &config);
+
+    println!("\nFinal balance: ${}", result.balance);
+    println!("{} trades made", result.trades.len());
 
     let total_returns = get_change(result.balance, config.balance);
     let duration = config.end_date - config.start_date;
     let years = duration.num_days() as f32 / 365.0;
     let annual_returns = total_returns / years;
 
-    println!("\nFinal balance: ${}", result.balance);
-    println!("Total returns: {}%", total_returns);
+    println!("\nTotal returns: {}%", total_returns);
     println!("Annual returns: {}%", annual_returns);
-    println!("{} trades made", result.trades.len());
 
     save_trades(&result.trades);
 }
